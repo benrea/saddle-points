7964bce00d1901e3cfad2d6044c5d96bae7857c3
Benjamin Rea <ben.rea@shuttlerock.com>
2022-09-16 14:42:46 +1200

    initial commit

---
+++ saddle-points.test.ts
@@ -0,0 +1,10 @@
+import { saddlePoints } from './saddle-points'
+
+describe('Saddle Points', () => {
+  it('Should...', () => {
+    const expected = 1
+    expect(
+      saddlePoints([])
+    ).toEqual(expected)
+  })
+})
